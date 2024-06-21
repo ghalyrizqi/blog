5c93d62f2be5864488f0ed4c5f6cb8fb51d21fb4
Ghaly Rizqi Mauludin <ghaly.rizqi@nusantara.technology>
2024-06-21 15:40:46 +0700

    miaw

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,18 +17,18 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              );
-            })}
+          <div className="flex flex-row space-x-0 ml-auto pr-10">
+            {" "}
+            {/* Changed ml-auto to align items to the right */}
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </nav>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,43 @@
 import { BlogPosts } from "app/components/posts";
+import { Inter, Poppins, Playfair_Display } from "next/font/google"; // Assuming PlayfairDisplay is imported similarly
+
+const inter = Inter({
+  weight: ["700"],
+  subsets: ["latin"],
+});
+const poppins = Poppins({
+  weight: ["300"],
+  subsets: ["latin"],
+});
+
+const poppins2 = Poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+const playfairDisplay = Playfair_Display({
+  weight: ["500"],
+  subsets: ["latin"],
+});
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+    <section className={inter.className}>
+      <h1 className="mb-8 text-6xl font-bold tracking-tighter">
+        <span className={`${inter.className} text-7xl`}>
+          Hello World!
+          <br></br>
+          I'm{" "}
+        </span>{" "}
+        <span
+          className={`${playfairDisplay.className} text-7xl underline decoration-4 underline-offset-8`}
+        >
+          Ghaly Rizqi
+        </span>
       </h1>
-      <p className="mb-4">
+      <p className={`${poppins.className} mb-8`}>
         {`Experienced Data Engineer with over 2 years of expertise in developing robust and scalable data solutions leveraging Python, Scala, SQL, and bash scripting. Proficient in utilizing Apache Spark, Apache Airflow, and Dbt for data pipeline construction and management. Successful track record in automation migration, ETL pipeline development, and forecasting model creation. Dedicated and skilled in delivering innovative solutions to drive positive impact.`}
       </p>
-      <br />
-      <div className="my-8">
+      <div className={`${poppins2.className} mb-8`}>
         <BlogPosts />
       </div>
     </section>
