f97bc5b7dd7cb8d6fec5c299192648663665ff26
Ghaly Rizqi Mauludin <ghaly.rizqi@nusantara.technology>
2024-06-21 21:35:24 +0700

    add logo and change font hero

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,12 +14,17 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex justify-between items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 ml-auto pr-10">
-            {" "}
-            {/* Changed ml-auto to align items to the right */}
+          {/* Left content: Logo text */}
+          <div className="left-content flex items-center">
+            <Link href="/" className="playwrite-us-trad text-3xl">
+              G
+            </Link>
+          </div>
+          {/* Right content: Navigation items */}
+          <div className="flex space-x-4">
             {Object.entries(navItems).map(([path, { name }]) => (
               <Link
                 key={path}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { BlogPosts } from "app/components/posts";
-import { Inter, Poppins, Playfair_Display } from "next/font/google"; // Assuming PlayfairDisplay is imported similarly
+import { Inter, Poppins } from "next/font/google"; // Assuming PlayfairDisplay is imported similarly
 
 const inter = Inter({
   weight: ["700"],
@@ -14,10 +14,6 @@ const poppins2 = Poppins({
   weight: ["400"],
   subsets: ["latin"],
 });
-const playfairDisplay = Playfair_Display({
-  weight: ["500"],
-  subsets: ["latin"],
-});
 
 export default function Page() {
   return (
@@ -25,13 +21,13 @@ export default function Page() {
       <h1 className="mb-8 text-6xl font-bold tracking-tighter">
         <span className={`${inter.className} text-7xl`}>
           Hello World!
-          <br></br>
+          <br />
           I'm{" "}
         </span>{" "}
         <span
-          className={`${playfairDisplay.className} text-7xl underline decoration-4 underline-offset-8`}
+          className={`playwrite-us-trad text-7xl underline decoration-4 underline-offset-8`}
         >
-          Ghaly Rizqi
+          ghaly
         </span>
       </h1>
       <p className={`${poppins.className} mb-8`}>
