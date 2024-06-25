408e6b8fc9e329bb3c885d48a22cc6d64022a48f
ghaly <ghaly.rizqi@nusantara.technology>
2024-06-25 22:38:37 +0700

    nutup tab resume

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,9 +7,9 @@ const navItems = {
   "/blog": {
     name: "blog",
   },
-  "/resume": {
-    name: "resume",
-  },
+  // "/resume": {
+  //   name: "resume",
+  // },
 };
 
 export function Navbar() {
