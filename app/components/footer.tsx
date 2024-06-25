a08a29430125ac8cdc42f2c1d7cb539a3e8ab46e
ghaly <ghaly.rizqi@nusantara.technology>
2024-06-25 22:55:07 +0700

    try

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -25,10 +25,10 @@ export default function Footer() {
               className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
               rel="noopener noreferrer"
               target="_blank"
-              href="/rss"
+              href="mailto:ghalyrizqimauludin@gmail.com"
             >
               <ArrowIcon />
-              <p className="ml-2 h-7">rss</p>
+              <p className="ml-2 h-7">email</p>
             </a>
           </li>
           <li>
