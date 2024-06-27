059115ecdec9791209697652f48380b6f7477e3d
ghaly <ghaly.rizqi@nusantara.technology>
2024-06-27 13:53:05 +0700

    fix errorrrr

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Add paths to your content files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
